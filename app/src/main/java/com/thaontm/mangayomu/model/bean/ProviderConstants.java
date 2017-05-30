f76391db6ba86919da7bc75d5c7e21edc62be09b
Hai Huynh <Hai.Huynh@mgm-tp.com>
2017-05-30 23:03:40 +0700

    Add ProviderConstants

---
+++ app/src/main/java/com/thaontm/mangayomu/model/bean/ProviderConstants.java
@@ -0,0 +1,12 @@
+package com.thaontm.mangayomu.model.bean;
+
+/**
+ * Created by noname
+ * on 29/05/2017.
+ */
+
+public interface ProviderConstants {
+    String CURRENT_PROVIDER = "CURRENT_PROVIDER";
+    int KAKALOT_PROVIDER = 1;
+    int MANGAPARK_PROVIDER = 2;
+}
